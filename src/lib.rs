a17a196698d13225170fb0852ee98a4d96a1e8fb
Hazel Bachrach <hbachrach@dropbox.com>
2023-04-24 19:37:53 -0700

    doc: add doc comments

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,83 @@
+#![warn(missing_docs)]
+
+//! A library that allows you to visualize tree data structures in Rust with
+//! output like `tree(1)`, like so:
+//!
+//! ```text
+//! Parent
+//! ├── Child 1
+//! ├── Child 2
+//! │   ├── Grandchild 1
+//! │   └── Grandchild 2
+//! └── Child 3
+//! ```
+//!
+//! This crate was extracted from [ruut](https://github.com/hibachrach/ruut), a CLI intended for doing the same
+//! thing. See that repo if you're interested in executing a tree visualizer from
+//! the commandline or for something that can process common serialized data types
+//! (e.g. JSON).
+
+/// Represents a node in the tree.
+///
+/// Important as [render] takes a [`Node`] as its only parameter.
+///
+/// # Example
+///
+/// ```
+/// use std::collections::VecDeque;
+/// use render_as_tree::Node;
+/// struct BasicNode {
+///     pub name: String,
+///     pub children: VecDeque<BasicNode>,
+/// }
+///
+/// impl BasicNode {
+///     pub fn new(name: String) -> BasicNode {
+///         BasicNode {
+///             name,
+///             children: VecDeque::new(),
+///         }
+///     }
+/// }
+///
+/// impl Node for BasicNode {
+///     type I<'a> = std::collections::vec_deque::Iter<'a, Self>;
+///
+///     fn name(&self) -> &str {
+///         &self.name
+///     }
+///     fn children(&self) -> Self::I<'_> {
+///         self.children.iter()
+///     }
+/// }
+/// ```
 pub trait Node {
+    /// An iterator over the children of this node
     type I<'a>: DoubleEndedIterator<Item = &'a Self>
     where
         Self: 'a;
 
+    /// What is displayed for this node when rendered
     fn name(&self) -> &str;
+    /// The immediate children of this node in the tree
     fn children(&self) -> Self::I<'_>;
 }
 
+/// Renders the given [`Node`] in a human-readable format
+///
+/// Here's an example:
+/// ```no_run
+/// vec![
+///     String::from("root - selena"),
+///     String::from("├── child 1 - sam"),
+///     String::from("│   ├── grandchild 1A - burt"),
+///     String::from("│   ├── grandchild 1B - crabbod"),
+///     String::from("│   └── grandchild 1C - mario"),
+///     String::from("└── child 2 - dumptruck"),
+///     String::from("    ├── grandchild 2A - tilly"),
+///     String::from("    └── grandchild 2B - curling iron"),
+/// ];
+/// ```
 pub fn render<T: Node>(node: &T) -> Vec<String> {
     let mut lines = vec![node.name().to_owned()];
     let mut children = node.children();
